b00269fe4f39493bae78284dc03111578541b7bc
kevdsouza_meli <keven.dsouza@mercadolivre.com>
2026-08-01 17:08:30 -0300

    Creating initial tokenizer

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"llm-golang/internal/tokenizer"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	token := tokenizer.NewTokenizer()
+	token.Train("O gato subiu no telhado o gato dormiu")
+
+	fmt.Println("A vocabulary size: ", token.VocabSize())
+
+	ids := token.Encode("O gato subiu")
+	fmt.Println(ids)
+
+	for _, id := range ids {
+		fmt.Println(token.Decode(id))
+	}
+
+	ids2 := token.Encode("O cachorro correu pelo telhado")
+
+	fmt.Println("Mensgaem enviada: o cachorro correu pelo telhado ")
+	fmt.Println("ids gerados: ", ids2)
+	fmt.Println("decode: ", token.Decode(ids2[0]))
 }
